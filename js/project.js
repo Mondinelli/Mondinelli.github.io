5b79befe5d4ecded8006c459da6f7d8d7eacd0ef
Mondinelli <mondinelli@gmail.com>
2022-08-15 14:54:40 +0200

    change the logo

--- js/project.js
+++ js/project.js
@@ -1,7 +1,7 @@
 let list = document.querySelectorAll('.list');
 let itemBox = document.querySelectorAll('.itemBox');
 
-for(let i= 0; i<list.length;i++){
+for(let i= 0;  i<list.length; i++){
     list[i].addEventListener('click', function(){
         for(let j = 0; j<list.length; j++){
             list[j].classList.remove('active');
